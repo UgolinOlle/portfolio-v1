1668e9d549660db819433673d1745a68927e3ca5
Ugolin Ollé <ugolin-olle@MacBook-Air-de-Ugolin.local>
2023-10-27 12:48:11 +0700

    style: add purple and yellow

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,38 +1,40 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-	content: [
-		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
-		"./components/**/*.{js,ts,jsx,tsx,mdx}",
-		"./app/**/*.{js,ts,jsx,tsx,mdx}",
-	],
-	theme: {
-		colors: {
-			blue: "#0171e2",
-			white: "#ffffff",
-			green: "#32C732",
-			red: "#FF6961"
-		},
-		extend: {
-			textColor: {
-				primary: "var(--gray-12)",
-				secondary: "var(--gray-11)",
-				tertiary: "var(--gray-9)",
-				brand: "var(--brand)",
-			},
-			backgroundColor: {
-				primary: "var(--gray-1)",
-				secondary: "var(--gray-4)",
-				secondaryA: "var(--grayA-4)",
-				tertiary: "var(--gray-3)",
-				blur: "var(--blurBackground)",
-			},
-			borderColor: {
-				primary: "var(--gray-6)",
-				secondary: "var(--gray-4)",
-			},
-		},
-	},
-	plugins: [],
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        colors: {
+            blue: "#0171e2",
+            white: "#ffffff",
+            green: "#32C732",
+            red: "#FF6961",
+            purple: "#800080",
+            yellow: "#FFBF00",
+        },
+        extend: {
+            textColor: {
+                primary: "var(--gray-12)",
+                secondary: "var(--gray-11)",
+                tertiary: "var(--gray-9)",
+                brand: "var(--brand)",
+            },
+            backgroundColor: {
+                primary: "var(--gray-1)",
+                secondary: "var(--gray-4)",
+                secondaryA: "var(--grayA-4)",
+                tertiary: "var(--gray-3)",
+                blur: "var(--blurBackground)",
+            },
+            borderColor: {
+                primary: "var(--gray-6)",
+                secondary: "var(--gray-4)",
+            },
+        },
+    },
+    plugins: [],
 };
 export default config;
