fffc1f26bd389cc021c2e90706583f179ab9c748
Ugolin Ollé <hello@ugolin-olle.com>
2023-11-27 16:37:19 +0700

    style: 💄 add more colors for future

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,16 +4,34 @@ const config: Config = {
     content: [
         "./pages/**/*.{js,ts,jsx,tsx,mdx}",
         "./components/**/*.{js,ts,jsx,tsx,mdx}",
-        "./app/**/*.{js,ts,jsx,tsx,mdx}",
     ],
     theme: {
         colors: {
+            // -- Commons
             blue: "#0171e2",
             white: "#ffffff",
             green: "#32C732",
-            red: "#FF6961",
+            red: "#ff6961",
             purple: "#800080",
-            yellow: "#FFBF00",
+            yellow: "#ffbf00",
+
+            // -- Grays
+            grayPrimary: "#f8f9fa", // Lightest Gray
+            graySecondary: "#e9ecef", // Light Gray
+            grayThird: "#dee2e6", // Medium Light Gray
+            grayFourth: "#ced4da", // Medium Gray
+            grayFifth: "#adb5bd", // Medium Dark Gray
+            graySixth: "#6c757d", // Dark Gray
+            graySeventh: "#495057", // Darker Gray
+            grayEighth: "#343a40", // Almost Black Gray
+
+            // -- Blacks
+            blackPrimary: "#282b30", // Lightest Black
+            blackSecondary: "#36393e", // Light Black
+            blackThird: "#424549", // Medium Black
+            blackFourth: "#212529", // Dark Black
+            blackFifth: "#1b1e21", // Darker Black
+            blackSixth: "#121416", // Darkest Black
         },
         extend: {
             textColor: {
@@ -35,6 +53,5 @@ const config: Config = {
             },
         },
     },
-    plugins: [],
 };
 export default config;
